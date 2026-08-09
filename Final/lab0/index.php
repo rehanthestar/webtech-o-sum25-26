106db424b50a9cc22e6a9f4ad44267a79726ccac
Rehan Khan <itpagla24@gmail.com>
2026-08-09 10:29:58 +0600

    Initiated git init

---
+++ Final/lab0/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+   
+<body>
+ 
+<h1>This is our 1st php class </h1>
+ 
+<?php
+echo"Hello World <br> ";
+ 
+$num=50;
+$num1= 40;
+$sum= $num+$num1;
+ 
+echo"The sum is ".$sum;
+ 
+echo "<br>";
+if($num>$num1)
+{
+    echo"print tata";
+}
+else{
+    echo"print bye bye";
+}
+
+
+echo"<h3> Ami Header 3 </h3>";
+ 
+?>
+ 
+</body>
+</html>

51e5265963f05b9a47aa60f879cff475c549fc9d
Rehan Khan <itpagla24@gmail.com>
2026-08-09 11:48:45 +0600

    HTML FORM Created

--- Final/lab1/index.php
+++ Final/lab1/index.php
@@ -12,6 +12,56 @@
     
 
 ?>
+
+
+<form  method ="POST" action="">
+
+    <label for="">Student Name: </label>
+    <input type="text" name="name" id="">
+
+        <br>
+        <br>
+
+    <label for="">Student Age: </label>
+    <input type="text" name="age" id="">
+        <br>
+        <br>
+
+    <label for="">University Email: </label>
+    <input type="text" name="email" id="">
+    <br>
+    <br>
+    <label for="">Membership Type: </label>
+
+    <input type="radio" name="rm" id="">
+    <label for="">Regular Member </label>
+    <input type="radio" name="em" id="">
+    <label for="">Excutive Member </label>
+    <input type="radio" name="v" id="">
+    <label for="">Volentiar </label>
+    
+    <br>
+    <br>
+    <label for="">Department: </label>
+    <select name="" id="">
+
+        <option value="">-- Select Department --</option>
+        <option value="">CSE</option>
+        <option value="">EEE</option>
+        <option value="">BBA</option>
+        <option value="">English</option>
+        <option value="">Architecture</option>
+    </select>
+
+    <br>
+    <br>
+    <label for="">Contact Number: </label>
+    <input type="text" name="" id="">
+    <br><br>
+    <input type="submit" value="Submit">
+    
+    
+</form>
  
 </body>
 </html>
